cab5bae50bffb521809c146303665dd723fa73d1
Kamal Nayan <88225844+Kamal725528@users.noreply.github.com>
2023-09-06 14:16:04 +0000

    working file

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,17 @@
 console.log("welcome kamal");
+
+const id=2;
+let name="kamal";
+var city="pune";
+age=22;
+let account;    // when declare but not assign its undefine
+
+// id=3     // const variable is not change
+
+name="nayan";
+city="hyderabad";
+age=25;
+
+/* we should avoid the use of var as there is issue in block and functional scope */
+
+console.table([name, city, age, account]);
