37dad039663d0346f011344ff760fc82a85b71f0
Kamal Nayan <88225844+Kamal725528@users.noreply.github.com>
2023-09-07 18:24:51 +0000

    change in committed

--- basic/data_type.js
+++ basic/data_type.js
@@ -7,11 +7,28 @@ let isLogin=true
 let roll;
 
 //string, number, bigInt, null, undefined, symbol---used to show uniqueness, widely used in react
-//object
+//object, array, functions -- reference/non-primitive type
 
-console.log(typeof "kamal")
-console.log(typeof null)  // is object
-console.log(typeof undefined) // is undefined
+// console.log(typeof "kamal")
+// console.log(typeof null)  // is object
+// console.log(typeof undefined) // is undefined
 
 // ECMA -- is a internation organisation to decide standard for js
+
+const id=Symbol('123');
+
+const bigNumber=9989877868757657n // n for bigInt
+
+const hero=["shaktiman",22, "batman"]
+
+const obj={name:"kamal", age:22};
+
+const myfunc=function(){
+    console.log("hello kamal")
+}
+
+console.log(myfunc())
+console.table([typeof bigNumber, typeof hero, typeof obj, typeof myfunc])  // bigInt, object, object, function
+
+console.log(typeof id) // symbol
 
