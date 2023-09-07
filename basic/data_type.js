c9c89ae2dff2a2ff4e19676f2037eb47ff9f6d0b
Kamal Nayan <88225844+Kamal725528@users.noreply.github.com>
2023-09-07 16:38:01 +0000

    datatype

---
+++ basic/data_type.js
@@ -0,0 +1,17 @@
+
+"use strict";  // earlier used for instructing interpreter to use modern js
+
+let name="kamal"
+let age=18
+let isLogin=true
+let roll;
+
+//string, number, bigInt, null, undefined, symbol---used to show uniqueness, widely used in react
+//object
+
+console.log(typeof "kamal")
+console.log(typeof null)  // is object
+console.log(typeof undefined) // is undefined
+
+// ECMA -- is a internation organisation to decide standard for js
+
